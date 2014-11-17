816874621fb8a9406a322ef5bca350f0cbbcbca2
sanyamkamat <sanyamkamat@gmail.com>
2014-11-17 11:55:04 +0530

    removing live reload functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,8 +68,8 @@ module.exports = function (grunt) {
       options: {
         port: 80,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
-        livereload: 35729
+        hostname: 'localhost'
+        //livereload: 35729
       },
       livereload: {
         options: {
